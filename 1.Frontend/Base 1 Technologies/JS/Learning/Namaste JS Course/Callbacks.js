db502fd873b6ed65b8ccc59a5ea1b9e13b4a4c9c
Sahil <sahilladhania5@gmail.com>
2024-08-01 02:39:02 +0530

    Added Few Folders.

---
+++ 1.Frontend/Base 1 Technologies/JS/Learning/Namaste JS Course/Callbacks.js
@@ -0,0 +1,10 @@
+console.log("From Callbacks.js File !!!");
+
+// What is Callback Function in JS.
+// JS is a Synchronous and Single Threaded Language.
+// Blocking the main thread.
+// Power of Callbacks.
+// Deep about Event Listeners.
+// Closures Demo with Event Listeners
+// Scope Demo with Event Listeners
+// Garbage Collection and Remove Event Listeners
