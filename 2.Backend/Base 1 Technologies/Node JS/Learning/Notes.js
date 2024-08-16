e899217ed872fa5254ee21734b823905b5c643f3
Sahil <sahilladhania5@gmail.com>
2024-08-16 23:56:23 +0530

    Made NodeJS Notes.

---
+++ 2.Backend/Base 1 Technologies/Node JS/Learning/Notes.js
@@ -0,0 +1,165 @@
+/*
+
+-----Introduction to NodeJS-----
+
+1. What is NodeJS ?
+Explanation :-
+2. Why use NodeJS ?
+Explanation :-
+3. What is the history of NodeJS ?
+Explanation :-
+4. What is the relation between NodeJS and Browser ?
+Explanation :-
+5. How to run NodeJS Code ?
+Explanation :-
+
+-----Modules-----
+
+1. What are Modules ?
+Explanation :-
+2. What is ESM Module ?
+Explanation :-
+3. What is CommonJS Module ?
+Explanation :-
+4. How to Create and Import Modules ?
+Explanation :-
+5. What is global Keyword ?
+Explanation :-
+
+-----npm-----
+
+1. What is npm ?
+Explanation :-
+2. What is npx ?
+Explanation :-
+3. What is Semantic Versioning ?
+Explanation :-
+4. What is Global Installation ?
+Explanation :-
+5. What is Local Installation ?
+Explanation :-
+6. How to update Packages ?
+Explanation :-
+7. How to Run Scripts ?
+Explanation :-
+8. What is npm Workspaces ?
+Explanation :-
+9. How to create Packages ?
+Explanation :-
+
+-----Error Handeling-----
+
+1. What is Error Handeling ?
+Explanation :-
+2. What is System Errors ?
+Explanation :-
+3. What is User Specified Errors ?
+Explanation :-
+4. What is Assertion Errors ?
+Explanation :-
+5. What is JS Errors ?
+Explanation :-
+6. What is Uncaught Exceptions ?
+Explanation :-
+7. How to Handle Async Errors ?
+Explanation :-
+8. What is Callstack / Stack Trace ?
+Explanation :-
+9. How to use Debuggers ?
+Explanation :-
+
+-----Async Programming-----
+
+1. What is Event Loop ?
+Explanation :-
+2. What is Event Emitter ?
+Explanation :-
+3. What are Promises ?
+Explanation :-
+4. What is async/await ?
+Explanation :-
+5. What are Callbacks ?
+Explanation :-
+6. What is setTimeout ?
+Explanation :-
+7. What is setInterval ?
+Explanation :-
+8. What is setImmediate ?
+Explanation :-
+9. What is process.nextTick ?
+Explanation :-
+
+-----Working with Files-----
+1. What is __dirname ?
+Explanation :-
+2. What is __filename ?
+Explanation :-
+3. What is process.cwd() ?
+Explanation :-
+4. What is path Module ?
+Explanation :-
+5. What is fs Module ?
+Explanation :-
+
+-----Command Line Apps-----
+
+1. What is Exitting / Exit Codes ?
+Explanation :-
+2. What are Environment Variables ?
+Explanation :-
+3. What is process.env ?
+Explanation :-
+4. What is dotenv Package ?
+Explanation :-
+5. How to take input ?
+Explanation :-
+6. What is process.stdin ?
+Explanation :-
+7. What is Inquirer Package ?
+Explanation :-
+8. What is Prompts Package ?
+Explanation :-
+9. How to print output ?
+Explanation :-
+10. What is stdout / stderr ?
+Explanation :-
+11. What is Chalk Package ?
+Explanation :-
+12. What is Figlet Package ?
+Explanation :-
+13. What is Cli progress Package ?
+Explanation :-
+14. What is Command Line args ?
+Explanation :-
+15. What is process.argv ?
+Explanation :-
+16. What is Commander.js ?
+Explanation :-
+
+-----Building and Consuming APIs-----
+
+1. What is jsonwebtoken ?
+Explanation :-
+2. What is http Module ?
+Explanation :-
+
+-----Monitor Changes-----
+
+1. What is Monitor Changes ?
+Explanation :-
+2. What is Nodemon ?
+Explanation :-
+
+-----Template Engines-----
+
+1. What are Template Engines ?
+Explanation :-
+2. What is ejs ?
+Explanation :-
+
+-----Working with DBs-----
+
+1. How to work with Prisma ORM in NodeJS ?
+Explanation :-
+
+*/
