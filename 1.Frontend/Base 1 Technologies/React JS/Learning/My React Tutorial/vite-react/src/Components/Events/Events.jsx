66390ad9cc3b7e80693c3c040fc5c5fd6214c9b1
Sahil <sahilladhania5@gmail.com>
2024-08-21 14:22:43 +0530

    Done Some Changes.

---
+++ 1.Frontend/Base 1 Technologies/React JS/Learning/My React Tutorial/vite-react/src/Components/Events/Events.jsx
@@ -0,0 +1,10 @@
+
+function Events() {
+    return (
+        <>
+            HI
+        </>
+    );
+}
+
+export default Events;
--- 1.Frontend/Base 1 Technologies/React JS/Learning/Practice React/mini-react-projects/src/App.jsx
+++ 1.Frontend/Base 1 Technologies/React JS/Learning/Practice React/mini-react-projects/src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css'
-import Accordian from "./Components/Accordian/Accordian.jsx";
-import RandomColourGenerator from "./Components/RandomColourGenerator/RandomColourGenerator.jsx";
-import StarRating from "./Components/StarRating/StarRating.jsx";
 
 function App() {
     return (
         <>
-            <Accordian/>
-            <RandomColourGenerator/>
-            <StarRating/>
+            Hi
         </>
     )
 }
