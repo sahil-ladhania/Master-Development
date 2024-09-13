80576ff81c178e7eb9f640e300b8e93b3632bf11
Sahil <sahilladhania5@gmail.com>
2024-09-13 18:16:26 +0530

    Done Some Changes.

--- 1.Frontend/Base 1 Technologies/React JS/Learning/Namaste React/Namaste React Code/namaste-food-delivery/src/components/OnlineRestaurantsContainerComponent.jsx
+++ 1.Frontend/Base 1 Technologies/React JS/Learning/Namaste React/Namaste React Code/namaste-food-delivery/src/components/OnlineRestaurantsContainerComponent.jsx
@@ -1,20 +1,21 @@
 import RestaurantCardComponent from "@/components/RestaurantCardComponent.jsx";
+import {useContext} from "react";
+import {OnlineRestaurantContext} from "@/contexts/onlineRestaurantContext.jsx";
 
 function OnlineRestaurantsContainerComponent() {
+    const {onlineRestaurantsState} = useContext(OnlineRestaurantContext);
+    console.log(onlineRestaurantsState);
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
-            <RestaurantCardComponent/>
+            {
+                onlineRestaurantsState.onlineRestaurants.length > 0 ?
+                    onlineRestaurantsState.onlineRestaurants.map((restaurant) => (
+                        <RestaurantCardComponent key={restaurant.info.id} restaurant={restaurant} />
+                    ))
+                    :
+                    <h1>Loading...</h1>
+            }
         </div>
     );
 }
